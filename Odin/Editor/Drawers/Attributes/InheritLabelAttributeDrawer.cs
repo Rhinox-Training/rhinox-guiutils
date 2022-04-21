606a02183bfd8a84110cef347f01b896d26628a7
Jorian Baeke <jorian@rhinox.be>
2022-04-21 09:55:50 +0200

    Initial attempt generic drawers - largest issue: generic serialization

---
+++ Odin/Editor/Drawers/Attributes/InheritLabelAttributeDrawer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+using UnityEngine;
+
+public class InheritLabelAttributeDrawer : OdinAttributeDrawer<InheritLabelAttribute>
+{
+    protected override void DrawPropertyLayout(GUIContent label)
+    {
+        label = Property.Parent.Label;
+        this.CallNextDrawer(label);
+    }
+}
