3ea24285c2c1f84b10e07daa82e752f3e4119941
Jorian Baeke <jorian@rhinox.training>
2022-12-15 14:45:34 +0100

    drawable factory

--- Assets/GUIUtils/Editor/BaseWindows/PagerEditorWindow.cs
+++ Assets/GUIUtils/Editor/BaseWindows/PagerEditorWindow.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using ICSharpCode.SharpZipLib.Zip;
 using Rhinox.GUIUtils.Editor;
 using Rhinox.GUIUtils.Editor.Helpers;
 using Rhinox.Lightspeed;
