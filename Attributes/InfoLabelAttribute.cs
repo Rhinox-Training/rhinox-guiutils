23ec3e22eaf10de8d0467a39cf767ba65a660dc6
Jorian Baeke <jorian@rhinox.training>
2022-12-15 14:16:31 +0100

    InfoLabelAttribute

---
+++ Attributes/InfoLabelAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class InfoLabelAttribute : Attribute
+{
+    public string Text;
+    // public string Icon;
+    public bool AlwaysShowAsTooltip;
+
+    public InfoLabelAttribute(string text)
+    {
+        Text = text;
+    }
+}
