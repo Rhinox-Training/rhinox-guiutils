94d63be23be07d3bf9b052624b7c9b657adaa631
Gaetan Deglorie <gaetan@rhinox.training>
2023-02-16 10:37:31 +0100

    [FIX] Fixes for list

--- Editor/GUI/Drawables/Entities/DrawableList.cs
+++ Editor/GUI/Drawables/Entities/DrawableList.cs
@@ -11,100 +11,92 @@ using Object = UnityEngine.Object;
 
 namespace Rhinox.GUIUtils.Editor
 {
-    public class DrawableList : SimpleUnityDrawable
+    public class ListElementDrawable
     {
-        private BetterReorderableList _listRO;
-        private readonly SerializedProperty _listProperty;
-        private readonly ListDrawerSettingsAttribute _listDrawerAttr;
-        
-        private ListElementDrawable[] _listElements;
-
-        private class ListElementDrawable
+        public float ElementHeight
         {
-            private readonly ICollection<IOrderedDrawable> _drawables;
-            private readonly SerializedProperty _property;
-
-            public float ElementHeight
+            get
             {
-                get
+                if (_property != null && _property.isExpanded)
                 {
-                    if (_property.isExpanded)
-                    {
-                        float height = EditorGUI.GetPropertyHeight(_property, true);
-                        return height;
-                    }
-                    else
-                    {
-                        return _defaultElementHeight;
-                    }
-                }
-            }
-
-            private readonly float _defaultElementHeight;
-            private readonly HostInfo _hostInfo;
-
-            public ListElementDrawable(SerializedProperty property, float defaultElementHeight = 18.0f)
-            {
-                if (property == null) throw new ArgumentNullException(nameof(property));
-                _defaultElementHeight = defaultElementHeight;
-                _property = property;
-                _hostInfo = property.GetHostInfo();
-                if (property.exposedReferenceValue != null)
-                    _drawables = DrawableFactory.ParseSerializedObject(new SerializedObject(property.exposedReferenceValue));
-            }
-
-            public void Draw(Rect r)
-            {
-                if (_drawables == null)
-                {
-                    var label = GUIContentHelper.TempContent(_hostInfo.GetReturnType().Name);
-                    EditorGUI.BeginProperty(r, label, _property);
-                    {
-                        EditorGUI.PropertyField(r, _property, label, true);
-                    }
-                    EditorGUI.EndProperty();
-                    // if (_property.isExpanded)
-                    // {
-                    //     float height = EditorGUI.GetPropertyHeight(_property, label, true);
-                    //     ElementHeight = height;
-                    // }
-                    // else
-                    // {
-                    //     ElementHeight = _defaultElementHeight;
-                    // }
-                    return;
+                    float height = EditorGUI.GetPropertyHeight(_property, true);
+                    return height;
                 }
 
-                foreach (var drawable in _drawables)
-                    drawable.Draw(r);
+                return _defaultElementHeight;
             }
         }
-        
+
+        private readonly ICollection<IOrderedDrawable> _drawables;
+        private readonly float _defaultElementHeight;
+        private readonly HostInfo _hostInfo;
+        private readonly SerializedProperty _property;
+
+        public ListElementDrawable(SerializedProperty property, float defaultElementHeight = 18.0f)
+        {
+            if (property == null) throw new ArgumentNullException(nameof(property));
+            _defaultElementHeight = defaultElementHeight;
+            _property = property;
+            _hostInfo = property.GetHostInfo();
+            if (property.exposedReferenceValue != null)
+                _drawables =
+                    DrawableFactory.ParseSerializedObject(new SerializedObject(property.exposedReferenceValue));
+        }
+
+        public ListElementDrawable(object element, float defaultElementHeight = 18.0f)
+        {
+            if (element == null) throw new ArgumentNullException(nameof(element));
+            _defaultElementHeight = defaultElementHeight;
+            _drawables = DrawableFactory.ParseNonUnityObject(element);
+        }
+
+        public void Draw(Rect r)
+        {
+            if (_drawables == null && _hostInfo != null && _property != null)
+            {
+                var label = GUIContentHelper.TempContent(_hostInfo.GetReturnType().Name);
+                EditorGUI.BeginProperty(r, label, _property);
+                {
+                    EditorGUI.PropertyField(r, _property, label, true);
+                }
+                EditorGUI.EndProperty();
+                return;
+            }
+
+            foreach (var drawable in _drawables)
+                drawable.Draw(r);
+        }
+    }
+
+    public class DrawableList : SimpleDrawable
+    {
+        private BetterReorderableList _listRO;
+        private readonly ListDrawerSettingsAttribute _listDrawerAttr;
+        private ListElementDrawable[] _listElements;
+
+        private readonly SerializedProperty _listProperty;
+        private readonly MemberInfo _listMemberInfo;
+        private readonly object _listContainerInstance;
+
         public DrawableList(SerializedProperty listProperty)
             : base(listProperty.serializedObject)
         {
             if (listProperty == null) throw new ArgumentNullException(nameof(listProperty));
             _listProperty = listProperty;
+            _listContainerInstance = null;
+            _listMemberInfo = null;
 
             _listDrawerAttr = listProperty.GetAttributeOrCreate<ListDrawerSettingsAttribute>(); // TODO: handle defaults
 
             _listRO = new PageableReorderableList(listProperty.serializedObject, listProperty,
-                _listDrawerAttr.DraggableItems, true, 
-                !_listDrawerAttr.IsReadOnly && !_listDrawerAttr.HideAddButton, !_listDrawerAttr.IsReadOnly && !_listDrawerAttr.HideRemoveButton)
+                _listDrawerAttr.DraggableItems, true,
+                !_listDrawerAttr.IsReadOnly && !_listDrawerAttr.HideAddButton,
+                !_listDrawerAttr.IsReadOnly && !_listDrawerAttr.HideRemoveButton)
             {
                 MaxItemsPerPage = _listDrawerAttr.NumberOfItemsPerPage
             };
-            
-            _listElements = new ListElementDrawable[_listRO.count];
-            for (int i = 0; i < _listRO.count; ++i)
-            {
-                _listElements[i] = new ListElementDrawable(_listProperty.GetArrayElementAtIndex(i));
-            }
-            
-            //_listRO.showDefaultBackground = false;
-            _listRO.drawElementCallback = DrawElement;
-            _listRO.onChangedCallback += OnChangedListCallback;
-            _listRO.elementHeightCallback = OnHeight;
+
+            Initialize(_listRO);
         }
 
         public DrawableList(object containerInstance, MemberInfo memberInfo)
@@ -112,26 +104,32 @@ namespace Rhinox.GUIUtils.Editor
         {
             if (containerInstance == null) throw new ArgumentNullException(nameof(containerInstance));
             _listProperty = null;
-
-            _listDrawerAttr = memberInfo.GetCustomAttribute<ListDrawerSettingsAttribute>() ?? new ListDrawerSettingsAttribute(); // TODO: handle defaults
+            _listContainerInstance = containerInstance;
+            _listMemberInfo = memberInfo;
+            _listDrawerAttr = memberInfo.GetCustomAttribute<ListDrawerSettingsAttribute>() ??
+                              new ListDrawerSettingsAttribute(); // TODO: handle defaults
 
             _listRO = new PageableReorderableList(memberInfo.GetValue(containerInstance) as IList,
-                _listDrawerAttr.DraggableItems, true, 
-                !_listDrawerAttr.IsReadOnly && !_listDrawerAttr.HideAddButton, !_listDrawerAttr.IsReadOnly && !_listDrawerAttr.HideRemoveButton)
+                _listDrawerAttr.DraggableItems, true,
+                !_listDrawerAttr.IsReadOnly && !_listDrawerAttr.HideAddButton,
+                !_listDrawerAttr.IsReadOnly && !_listDrawerAttr.HideRemoveButton)
             {
                 MaxItemsPerPage = _listDrawerAttr.NumberOfItemsPerPage
             };
-            
-            _listElements = new ListElementDrawable[_listRO.count];
-            for (int i = 0; i < _listRO.count; ++i)
-            {
-                _listElements[i] = new ListElementDrawable(_listProperty.GetArrayElementAtIndex(i));
-            }
-            
+
+            Initialize(_listRO);
+        }
+
+        private void Initialize(BetterReorderableList roList)
+        {
+            _listElements = new ListElementDrawable[roList.count];
+            for (int i = 0; i < roList.count; ++i)
+                _listElements[i] = CreateElementFor(i);
+
             //_listRO.showDefaultBackground = false;
-            _listRO.drawElementCallback = DrawElement;
-            _listRO.onChangedCallback += OnChangedListCallback;
-            _listRO.elementHeightCallback = OnHeight;
+            roList.drawElementCallback = DrawElement;
+            roList.onChangedCallback += OnChangedListCallback;
+            roList.elementHeightCallback = OnHeight;
         }
 
         private float OnHeight(int index)
@@ -144,21 +142,21 @@ namespace Rhinox.GUIUtils.Editor
             return _listRO.elementHeight;
         }
 
-        protected override void Draw(UnityEngine.Object target)
+        protected override void Draw(object target)
         {
-            if (_listProperty != null && _listProperty.serializedObject != null)
+            if (_listRO != null && _listDrawerAttr != null)
             {
-                _listProperty.serializedObject.Update();
+                OnBeginDraw();
                 EditorGUI.BeginDisabledGroup(_listDrawerAttr.IsReadOnly);
                 {
                     _listRO.DoLayoutList();
                 }
                 EditorGUI.EndDisabledGroup();
-                _listProperty.serializedObject.ApplyModifiedProperties();
+                OnEndDraw();
             }
         }
 
-        protected override void Draw(Rect rect, Object target)
+        protected override void Draw(Rect rect, object target)
         {
             if (_listProperty != null && _listProperty.serializedObject != null)
             {
@@ -176,16 +174,51 @@ namespace Rhinox.GUIUtils.Editor
         {
             _listElements = new ListElementDrawable[list.count];
         }
-        
+
         private void DrawElement(Rect rect, int index, bool isActive, bool isFocused)
         {
             var listEntryRect = _listDrawerAttr.IsReadOnly ? rect : rect.AlignLeft(rect.width - 16);
-            
+
             if (_listElements[index] == null)
-                _listElements[index] = new ListElementDrawable(_listRO.serializedProperty.GetArrayElementAtIndex(index), _listRO.elementHeight);
+                _listElements[index] = CreateElementFor(index);
             _listElements[index].Draw(listEntryRect);
-            
-            //EditorGUI.PropertyField(listEntryRect, _listRO.serializedProperty.GetArrayElementAtIndex(index), GUIContent.none);
+        }
+
+        private ListElementDrawable CreateElementFor(int index)
+        {
+            if (_listProperty != null)
+            {
+                var element = _listProperty.GetArrayElementAtIndex(index);
+                return new ListElementDrawable(element, _listRO.elementHeight);
+            }
+            else
+            {
+                var value = _listMemberInfo.GetValue(_listContainerInstance) as IList;
+                var nonUnityElement = value[index];
+                return new ListElementDrawable(nonUnityElement, _listRO.elementHeight);
+            }
+        }
+
+        private void OnBeginDraw()
+        {
+            if (_listProperty != null)
+            {
+                if (_listProperty.serializedObject != null)
+                    _listProperty.serializedObject.Update();
+            }
+        }
+
+        private void OnEndDraw()
+        {
+            if (_listProperty != null)
+            {
+                if (_listProperty.serializedObject != null)
+                    _listProperty.serializedObject.ApplyModifiedProperties();
+            }
+            else
+            {
+                _listMemberInfo.SetValue(_listContainerInstance, _listRO.list);
+            }
         }
     }
 }
