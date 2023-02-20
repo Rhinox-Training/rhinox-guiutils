5e2c4c253c50fbd228bfd1d3e3e211661a2a04cd
Gaetan Deglorie <gaetan@rhinox.training>
2023-02-20 15:35:05 +0100

    [MOD] File moves

---
+++ Assets/GUIUtils/Editor/GUI/Data/GUIButton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rhinox.GUIUtils.Editor
+{
+    public class GUIButton
+    {
+        public GUIContent Label;
+        public Func<bool> _canExecute;
+        public Action _action;
+        
+        public GUIButton(string label, Func<bool> canExecute, Action action, string tooltip = null)
+            : this(new GUIContent(label, tooltip), canExecute, action)
+        { }
+        
+        public GUIButton(Texture tex, Func<bool> canExecute, Action action, string tooltip = null)
+            : this(new GUIContent(tex, tooltip), canExecute, action)
+        { }
+        
+        public GUIButton(Texture tex, string label, Func<bool> canExecute, Action action, string tooltip = null)
+            : this(new GUIContent(label, tex, tooltip), canExecute, action)
+        { }
+        
+        public GUIButton(GUIContent label, Func<bool> canExecute, Action action)
+        {
+            Label = label;
+            
+            _canExecute = canExecute;
+            _action = action;
+        }
+
+        public void Execute()
+        {
+            _action.Invoke();
+        }
+        
+        public bool CanExecute() => _canExecute.Invoke();
+    }
+}
--- Assets/GUIUtils/Editor/GUI/Data/GUIButtonList.cs
+++ Assets/GUIUtils/Editor/GUI/Data/GUIButtonList.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Rhinox.GUIUtils.Editor
@@ -9,7 +8,7 @@ namespace Rhinox.GUIUtils.Editor
         private int _buttonsDrawn;
 
         public GUIButtonList(IEnumerable<GUIButton> collection)
-            : base(collection)
+            : base(collection)  
         {
         }
 
@@ -52,39 +51,5 @@ namespace Rhinox.GUIUtils.Editor
             // but due to the layout/repaint loop; this is never visible
             _buttonsDrawn = buttonsDrawn;
         }
-    }
-    
-    public class GUIButton
-    {
-        public GUIContent Label;
-        public Func<bool> _canExecute;
-        public Action _action;
-        
-        public GUIButton(string label, Func<bool> canExecute, Action action, string tooltip = null)
-            : this(new GUIContent(label, tooltip), canExecute, action)
-        { }
-        
-        public GUIButton(Texture tex, Func<bool> canExecute, Action action, string tooltip = null)
-            : this(new GUIContent(tex, tooltip), canExecute, action)
-        { }
-        
-        public GUIButton(Texture tex, string label, Func<bool> canExecute, Action action, string tooltip = null)
-            : this(new GUIContent(label, tex, tooltip), canExecute, action)
-        { }
-        
-        public GUIButton(GUIContent label, Func<bool> canExecute, Action action)
-        {
-            Label = label;
-            
-            _canExecute = canExecute;
-            _action = action;
-        }
-
-        public void Execute()
-        {
-            _action.Invoke();
-        }
-        
-        public bool CanExecute() => _canExecute.Invoke();
     }
 }
