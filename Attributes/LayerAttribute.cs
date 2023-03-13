0733fda0c7f4b3114a6bc279612021506e605ab8
Gaetan Deglorie <gaetan@rhinox.training>
2023-03-13 16:58:35 +0100

    [FIX] fix

--- Attributes/LayerAttribute.cs
+++ Attributes/LayerAttribute.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using UnityEngine;
 
 namespace Rhinox.GUIUtils.Attributes
 {
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
     [Conditional("UNITY_EDITOR")]
-    public class LayerAttribute : Attribute
+    public class LayerAttribute : PropertyAttribute
     { 
     }
 }
