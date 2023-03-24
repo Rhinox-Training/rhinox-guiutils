089088440441cec010030b32ef4fa1b4e42c372c
Gaetan Deglorie <gaetan@rhinox.training>
2023-03-24 18:06:53 +0100

    [MOD] Rect implementations

--- Editor/GUI/Drawables/Wrappers/InfoBoxWrapper.cs
+++ Editor/GUI/Drawables/Wrappers/InfoBoxWrapper.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Reflection;
+using Rhinox.Lightspeed;
 using Sirenix.OdinInspector;
 using UnityEditor;
 using UnityEngine;
@@ -10,16 +12,55 @@ namespace Rhinox.GUIUtils.Editor
         private IPropertyMemberHelper<bool> _member;
         private string _message;
         private MessageType _type;
-        
+        private MethodInfo _getHelpIconMethod;
+        private float _newHeight;
+
+        public override float ElementHeight
+        {
+            get
+            {
+                return base.ElementHeight + _newHeight;
+            }
+        }
+
         public InfoBoxWrapper(IOrderedDrawable drawable) : base(drawable)
         {
         }
 
-        protected override void OnPreDraw()
+        protected override void DrawInner(GUIContent label)
         {
             if (_member == null || _member.GetValue())
                 EditorGUILayout.HelpBox(_message, _type);
-            base.OnPreDraw();
+            base.DrawInner(label);
+        }
+
+        protected override void DrawInner(Rect rect, GUIContent label)
+        {
+            if (_member == null || _member.GetValue())
+            {
+                _newHeight = CalcMessageHeight(rect.width);
+                var helpBoxRect = rect.AlignTop(_newHeight);
+                
+                EditorGUI.HelpBox(helpBoxRect, _message, _type);
+                rect.y += helpBoxRect.height;
+                rect.height -= helpBoxRect.height;
+            }
+
+            base.DrawInner(rect, label);
+        }
+
+        private float CalcMessageHeight(float width)
+        {
+            if (_getHelpIconMethod == null)
+            {
+                _getHelpIconMethod = typeof(EditorGUIUtility).GetMethod("GetHelpIcon",
+                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+            }
+
+            var icon = _getHelpIconMethod.Invoke(null, new object[] {_type}) as Texture2D;
+            
+            return EditorStyles.helpBox.CalcHeight(
+                GUIContentHelper.TempContent(_message, (Texture)icon), width);
         }
 
         [WrapDrawer(typeof(InfoBoxAttribute), -5000)]
