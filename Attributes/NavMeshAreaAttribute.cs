16ab573c234e7fea0aa9b17a7e933e8d937698f7
Gaetan Deglorie <gaetan@rhinox.training>
2023-03-24 09:58:54 +0100

    [ADD] NavMeshAreaAttribute and drawer (works  for both masks and layer index)

---
+++ Attributes/NavMeshAreaAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace Rhinox.GUIUtils.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    [Conditional("UNITY_EDITOR")]
+    public class NavMeshAreaAttribute : PropertyAttribute
+    {
+        public bool IsMask { get; }
+        
+        public NavMeshAreaAttribute(bool mask = false)
+        {
+            IsMask = mask;
+        }
+    }
+}
