86eefa59b39932ec3f3c6b7e331f4b8601538534
Jorian Baeke <jorian@rhinox.training>
2023-04-04 15:41:27 +0200

    [MOD] Fix groups (again)

--- Editor/GUI/Drawables/Composite/PropertyGroupDrawable.cs
+++ Editor/GUI/Drawables/Composite/PropertyGroupDrawable.cs
@@ -8,8 +8,8 @@ namespace Rhinox.GUIUtils.Editor
     public abstract class PropertyGroupDrawable<T> : GroupedDrawable
         where T : PropertyGroupAttribute
     {
-        protected readonly List<T> _parsedAttributes = new List<T>();
-
+        protected List<T> _groupAttributes = new List<T>();
+        
         protected PropertyGroupDrawable(GroupedDrawable parent, string groupID, float order) : base(parent, groupID, order)
         {
             
@@ -28,29 +28,28 @@ namespace Rhinox.GUIUtils.Editor
         {
             var groupAttribute = ValidateAttribute(attr);
 
+            _groupAttributes.Add(groupAttribute);
+            
             // No need to trigger ParseAttribute -> should have happened already, but it can't hurt
             ParseAttribute(groupAttribute);
             ParseAttribute(child, groupAttribute);
         }
 
         protected abstract void ParseAttribute(IOrderedDrawable child, T attr);
-
-        
-        protected override void ParseAttribute(PropertyGroupAttribute attr)
-        {
-            if (_parsedAttributes.Contains(attr))
-                return;
-
-            var groupAttribute = ValidateAttribute(attr);
-            _parsedAttributes.Add(groupAttribute);
-            ParseAttribute(groupAttribute);
-        }
-        
         protected abstract void ParseAttribute(T attr);
 
-        public override IEnumerable<TAttribute> GetDrawableAttributes<TAttribute>()
+        public override void AddAttribute(Attribute attr)
         {
-            return _parsedAttributes.OfType<TAttribute>();
+            if (_attributes.Contains(attr))
+                return;
+            
+            base.AddAttribute(attr);
+
+            if (attr is PropertyGroupAttribute groupAttribute)
+            {
+                var typedGroupAttribute = ValidateAttribute(groupAttribute);
+                ParseAttribute(typedGroupAttribute);
+            }
         }
     }
 }
