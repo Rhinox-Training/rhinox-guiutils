67268b4cc87a5b93ae64cfc26c7b6fd7cce03e34
Jorian Baeke <jorian@rhinox.training>
2023-04-06 14:22:58 +0200

    [MOD] remove ToggleLeft support since it is now a wrapper

--- Assets/GUIUtils/Editor/GUI/Drawables/Members/BoolDrawableField.cs
+++ Assets/GUIUtils/Editor/GUI/Drawables/Members/BoolDrawableField.cs
@@ -9,29 +9,17 @@ namespace Rhinox.GUIUtils.Editor
 {
     public class BoolDrawableField : BaseMemberDrawable<bool>
     {
-        private bool _left;
-        
         public BoolDrawableField(GenericMemberEntry entry) : base(entry)
         {
         }
 
-        protected override void Initialize()
-        {
-            _left = GetDrawableAttribute<ToggleLeftAttribute>() != null;
-            base.Initialize();
-        }
-
         protected override bool DrawValue(GUIContent label, bool val, params GUILayoutOption[] options)
         {
-            if (_left)
-                return EditorGUILayout.ToggleLeft(label, val, CustomGUIStyles.CleanLabelField, options);
             return EditorGUILayout.Toggle(label, val, CustomGUIStyles.CleanLabelField, options);
         }
 
         protected override bool DrawValue(Rect rect, GUIContent label, bool val)
         {
-            if (_left)
-                return EditorGUI.ToggleLeft(rect, label, val);
             return EditorGUI.Toggle(rect, label, val);
         }
     }
