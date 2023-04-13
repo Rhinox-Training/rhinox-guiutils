af4708599864b39119df186abc2c13f96a4e9e90
Gaetan Deglorie <gaetan@rhinox.training>
2023-04-13 11:49:41 +0200

    [ADD] DisplayAsStringAligned

---
+++ Attributes/DisplayAsStringAlignedAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Rhinox.GUIUtils.Attributes
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)] 
+    public class DisplayAsStringAlignedAttribute : PropertyAttribute
+    {
+        public TextAlignment Alignment { get; }
+        
+        public bool Overflow { get; }
+
+        public DisplayAsStringAlignedAttribute(bool overflow = true)
+        {
+            Alignment = TextAlignment.Left;
+            Overflow = overflow;
+        }
+
+        public DisplayAsStringAlignedAttribute(TextAlignment alignment, bool overflow = true)
+        {
+            Alignment = alignment;
+            Overflow = overflow;
+        }
+    }
+}
