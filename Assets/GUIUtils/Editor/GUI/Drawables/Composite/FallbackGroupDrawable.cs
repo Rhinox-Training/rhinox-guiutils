8cb01796538daf483d01982fa519810932a2f406
Gaetan Deglorie <gaetan@rhinox.training>
2023-04-20 16:24:33 +0200

    [ADD] FallbackGroupDrawable for handling unsupported groups (instead of just breaking the editor)

---
+++ Assets/GUIUtils/Editor/GUI/Drawables/Composite/FallbackGroupDrawable.cs
@@ -0,0 +1,26 @@
+﻿using Sirenix.OdinInspector;
+
+namespace Rhinox.GUIUtils.Editor
+{
+    public class FallbackGroupDrawable : BaseVerticalGroupDrawable<PropertyGroupAttribute>
+    {
+        public FallbackGroupDrawable(GroupedDrawable parent, string groupID, float order) 
+            : base(parent, groupID, order)
+        {
+        }
+
+
+        protected override void ParseAttributeSmart(IOrderedDrawable child, PropertyGroupAttribute attr)
+        {
+            // NOP
+        }
+
+        protected override void ParseAttributeSmart(PropertyGroupAttribute attr)
+        {
+            SetOrder(attr.Order);
+            
+            // TODO
+            _parent?.EnsureSizeFits(_size);
+        }
+    }
+}
