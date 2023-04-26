c1381a32616a153ffcbd06b1c0eb8e5bebe9df3d
Jorian Baeke <jorian@rhinox.training>
2023-04-26 12:58:56 +0200

    [ADD] BasePropertyDrawer

---
+++ Assets/GUIUtils/Editor/Drawers/BasePropertyDrawer.cs
@@ -0,0 +1,58 @@
+using Rhinox.Lightspeed;
+using UnityEditor;
+using UnityEngine;
+
+namespace Rhinox.GUIUtils.Editor
+{
+    public abstract class ValuePropertyDrawer<T> : BasePropertyDrawer
+    {
+        private HostInfo _hostInfo;
+        
+        public T SmartValue
+        {
+            get => _hostInfo.GetSmartValue<T>();
+            set => _hostInfo.TrySetValue(value);
+        }
+        
+        protected override void Initialize()
+        {
+            _hostInfo = Property.GetHostInfo();
+            
+            base.Initialize();
+        }
+    }
+    
+    public abstract class BasePropertyDrawer : PropertyDrawer
+    {
+        protected SerializedProperty Property;
+        
+        private bool _initialized;
+        protected Rect _rect;
+        
+        protected virtual void Initialize() {}
+        protected abstract void DrawPropertyLayout(GUIContent label);
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            Property = property;
+            
+            if (!_initialized)
+            {
+                Initialize();
+                _initialized = true;
+            }
+
+            var rect = EditorGUILayout.BeginVertical();
+
+            DrawPropertyLayout(label);
+            
+            EditorGUILayout.EndVertical();
+
+            if (rect.IsValid())
+                _rect = rect;
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+            => _rect.height;
+    }
+}
