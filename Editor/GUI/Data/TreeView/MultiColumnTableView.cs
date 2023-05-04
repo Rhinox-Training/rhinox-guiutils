4fadac5dbbf6a6ce64f2df1702d26f81ce93ba46
Gaetan Deglorie <gaetan@rhinox.training>
2023-05-04 14:46:27 +0200

    [ADD] TreeView WIP

---
+++ Editor/GUI/Data/TreeView/MultiColumnTableView.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Rhinox.GUIUtils;
+using Rhinox.Lightspeed;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace Rhinox.GUIUtils.Editor
+{
+    public abstract class MultiColumnTableView<T> : TreeViewWithTreeModel<T> where T : TreeElement
+    {
+        const float kRowHeights = 20f;
+        const float kToggleWidth = 18f;
+        public bool showControls = true;
+
+        public static void TreeToList(TreeViewItem root, IList<TreeViewItem> result)
+        {
+            if (root == null)
+                throw new NullReferenceException("root");
+            if (result == null)
+                throw new NullReferenceException("result");
+
+            result.Clear();
+
+            if (root.children == null)
+                return;
+
+            Stack<TreeViewItem> stack = new Stack<TreeViewItem>();
+            for (int i = root.children.Count - 1; i >= 0; i--)
+                stack.Push(root.children[i]);
+
+            while (stack.Count > 0)
+            {
+                TreeViewItem current = stack.Pop();
+                result.Add(current);
+
+                if (current.hasChildren && current.children[0] != null)
+                {
+                    for (int i = current.children.Count - 1; i >= 0; i--)
+                    {
+                        stack.Push(current.children[i]);
+                    }
+                }
+            }
+        }
+
+        public MultiColumnTableView(TreeViewState state, MultiColumnHeader multicolumnHeader, TreeModel<T> model)
+            : base(state, multicolumnHeader, model)
+        {
+            // Custom setup
+            rowHeight = kRowHeights;
+            columnIndexForTreeFoldouts = 2;
+            showAlternatingRowBackgrounds = true;
+            showBorder = true;
+            customFoldoutYOffset =
+                (kRowHeights - EditorGUIUtility.singleLineHeight) *
+                0.5f; // center foldout in the row since we also center content. See RowGUI
+            extraSpaceBeforeIconAndLabel = kToggleWidth;
+            multicolumnHeader.sortingChanged += OnSortingChanged;
+
+            Reload();
+        }
+
+
+        // Note we We only build the visible rows, only the backend has the full tree information. 
+        // The treeview only creates info for the row list.
+        protected override IList<TreeViewItem> BuildRows(TreeViewItem root)
+        {
+            var rows = base.BuildRows(root);
+            SortIfNeeded(root, rows);
+            return rows;
+        }
+
+        void OnSortingChanged(MultiColumnHeader multiColumnHeader)
+        {
+            SortIfNeeded(rootItem, GetRows());
+        }
+
+        void SortIfNeeded(TreeViewItem root, IList<TreeViewItem> rows)
+        {
+            if (rows.Count <= 1)
+                return;
+
+            if (multiColumnHeader.sortedColumnIndex == -1)
+            {
+                return; // No column to sort for (just use the order the data are in)
+            }
+
+            // Sort the roots of the existing tree items
+            SortByMultipleColumns();
+            TreeToList(root, rows);
+            Repaint();
+        }
+
+        void SortByMultipleColumns()
+        {
+            var sortedColumns = multiColumnHeader.state.sortedColumns;
+
+            if (sortedColumns.Length == 0)
+                return;
+
+            var myTypes = rootItem.children.Cast<TreeViewItem<T>>();
+            var orderedQuery = InitialOrder(myTypes, sortedColumns);
+            for (int i = 1; i < sortedColumns.Length; i++)
+            {
+                bool ascending = multiColumnHeader.IsSortedAscending(sortedColumns[i]);
+                orderedQuery = orderedQuery.ThenBy(l => GetCellData(l, sortedColumns[i]), ascending);
+            }
+
+            rootItem.children = orderedQuery.Cast<TreeViewItem>().ToList();
+        }
+
+        IOrderedEnumerable<TreeViewItem<T>> InitialOrder(IEnumerable<TreeViewItem<T>> myTypes, int[] history)
+        {
+            bool ascending = multiColumnHeader.IsSortedAscending(history[0]);
+            // default
+            return myTypes.Order(l => l.data.name, ascending);
+        }
+
+        protected abstract object GetCellData(TreeViewItem<T> row, int columnIndex);
+
+        protected override void RowGUI(RowGUIArgs args)
+        {
+            var item = (TreeViewItem<T>) args.item;
+
+            for (int i = 0; i < args.GetNumVisibleColumns(); ++i)
+            {
+                CellGUI(args.GetCellRect(i), item, args.GetColumn(i), ref args);
+            }
+        }
+
+        protected virtual void RenderCellEntry(Rect cellRect, TreeViewItem<T> item, int columnIndex,
+            ref RowGUIArgs args)
+        {
+            var data = GetCellData(item, columnIndex);
+            GUI.Label(cellRect, GUIContentHelper.TempContent(data.ToString()));
+        }
+
+        private void CellGUI(Rect cellRect, TreeViewItem<T> item, int columnIndex, ref RowGUIArgs args)
+        {
+            // Center cell rect vertically (makes it easier to place controls, icons etc in the cells)
+            CenterRectUsingSingleLineHeight(ref cellRect);
+
+            RenderCellEntry(cellRect, item, columnIndex, ref args);
+        }
+
+        // Rename
+        //--------
+
+        protected override bool CanRename(TreeViewItem item)
+        {
+            // Only allow rename if we can show the rename overlay with a certain width (label might be clipped by other columns)
+            Rect renameRect = GetRenameRect(treeViewRect, 0, item);
+            return renameRect.width > 30;
+        }
+
+        protected override void RenameEnded(RenameEndedArgs args)
+        {
+            // Set the backend name and reload the tree to reflect the new model
+            if (args.acceptedRename)
+            {
+                var element = treeModel.Find(args.itemID);
+                element.name = args.newName;
+                Reload();
+            }
+        }
+
+        protected override Rect GetRenameRect(Rect rowRect, int row, TreeViewItem item)
+        {
+            Rect cellRect = GetCellRectForTreeFoldouts(rowRect);
+            CenterRectUsingSingleLineHeight(ref cellRect);
+            return base.GetRenameRect(cellRect, row, item);
+        }
+
+        // Misc
+        //--------
+
+        protected override bool CanMultiSelect(TreeViewItem item)
+        {
+            return true;
+        }
+    }
+}
