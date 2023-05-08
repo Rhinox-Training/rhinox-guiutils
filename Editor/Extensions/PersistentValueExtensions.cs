a5aa0190dd427968a02a4488d7fd9e5684785115
Gaetan Deglorie <gaetan@rhinox.training>
2023-05-08 13:21:12 +0200

    [ADD] ShowField for integer and string + optional layoutoption parameters

--- Editor/Extensions/PersistentValueExtensions.cs
+++ Editor/Extensions/PersistentValueExtensions.cs
@@ -1,18 +1,31 @@
 using UnityEditor;
+using UnityEngine;
 
 namespace Rhinox.GUIUtils.Editor
 {
     public static class PersistentValueExtensions
     {
-        public static bool ShowField(this PersistentValue<bool> val, string label)
+        public static bool ShowField(this PersistentValue<bool> val, string label, params GUILayoutOption[] layoutOptions)
         {
-            var result = EditorGUILayout.Toggle(label, val);
+            var result = EditorGUILayout.Toggle(label, val, layoutOptions);
             return val.Set(result);
         }
         
-        public static bool ShowField(this PersistentValue<float> val, string label)
+        public static bool ShowField(this PersistentValue<float> val, string label, params GUILayoutOption[] layoutOptions)
         {
-            var result = EditorGUILayout.FloatField(label, val);
+            var result = EditorGUILayout.FloatField(label, val, layoutOptions);
+            return val.Set(result);
+        }
+        
+        public static bool ShowField(this PersistentValue<int> val, string label, params GUILayoutOption[] layoutOptions)
+        {
+            var result = EditorGUILayout.IntField(label, val, layoutOptions);
+            return val.Set(result);
+        }
+        
+        public static bool ShowField(this PersistentValue<string> val, string label, params GUILayoutOption[] layoutOptions)
+        {
+            var result = EditorGUILayout.TextField(label, val, layoutOptions);
             return val.Set(result);
         }
     }
