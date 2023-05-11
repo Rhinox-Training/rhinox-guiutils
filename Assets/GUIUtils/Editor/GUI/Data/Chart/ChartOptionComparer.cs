5a86bf5d779635a58a490f23cd239c1cc8c1ec35
Gaetan Deglorie <gaetan@rhinox.training>
2023-05-11 10:53:21 +0200

    [ADD] Chart drawer

---
+++ Assets/GUIUtils/Editor/GUI/Data/Chart/ChartOptionComparer.cs
@@ -0,0 +1,46 @@
+﻿/**
+ * Copyright (c) 2019 Alessandro Tironi
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLRDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USER OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using System.Collections.Generic;
+
+namespace Rhinox.GUIUtils.Editor
+{
+	/// <summary>
+	/// Sorts chart options according to their priority.
+	/// This is necessary since the application order of chart options can
+	/// break the chart.
+	/// </summary>
+	public class ChartOptionComparer : IComparer<ChartOption>
+	{
+		public int Compare(ChartOption x, ChartOption y)
+		{
+			if (x == y || x == null || y == null)
+				return 0;
+			if (x.GetType() == y.GetType())
+				return 0;
+			return x.Priority - y.Priority;
+		}
+	}
+}
