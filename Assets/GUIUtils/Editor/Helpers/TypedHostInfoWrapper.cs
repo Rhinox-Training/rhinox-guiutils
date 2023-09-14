ef505bbadcd5e3c5c55d62dba65bfcde6b7ad128
Gaetan Deglorie <gaetan@rhinox.training>
2023-09-14 11:17:41 +0200

    [MOD] HostInfo

---
+++ Assets/GUIUtils/Editor/Helpers/TypedHostInfoWrapper.cs
@@ -0,0 +1,17 @@
+﻿namespace Rhinox.GUIUtils.Editor
+{
+    public class TypedHostInfoWrapper<T>
+    {
+        public GenericHostInfo HostInfo { get;}
+
+        public T SmartValue {
+            get { return HostInfo.GetSmartValue<T>(); }
+            set { HostInfo.SetValue(value); }
+        }
+        
+        public TypedHostInfoWrapper(GenericHostInfo hostInfo)
+        {
+            HostInfo = hostInfo;
+        }
+    }
+}
