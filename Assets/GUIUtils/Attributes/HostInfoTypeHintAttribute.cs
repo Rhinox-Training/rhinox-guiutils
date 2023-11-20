52cecbc361d623f7bb11aafa907b68e4a6f53dbd
Gaetan Deglorie <gaetan@rhinox.training>
2023-11-20 16:26:26 +0100

    [ADD] HostInfo type hinting
    [ADD] HostInfo CanSetValue (for drawer parsing help)
    [MOD] HostInfo can now create childHostInfos for array elements

---
+++ Assets/GUIUtils/Attributes/HostInfoTypeHintAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Rhinox.Lightspeed;
+
+namespace Rhinox.GUIUtils.Attributes
+{
+    public class HostInfoTypeHintAttribute : Attribute
+    {
+        public string Member { get; }
+
+        public HostInfoTypeHintAttribute(string member)
+        {
+            Member = member;
+        }
+    }
+}
